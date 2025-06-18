927fdbd8d29df70960429ba5e37adae4232e03a4
ohm <pontakorn_oom@hotmail.co.th>
2025-06-18 12:21:30 +0700

    ggg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
   base: '/',
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: import.meta.env.PROD 
+        target: command === 'build' 
           ? 'https://backend-ifp88by28-ohms-projects-4b3e1e96.vercel.app'
           : 'http://localhost:8080',
         changeOrigin: true,
@@ -27,4 +28,4 @@ export default defineConfig({
       }
     }
   }
-}) 
+})) 
